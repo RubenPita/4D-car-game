25bb527108a3825206227e7777d710d22031efee
RubenZombiastic <ruben.pita@hotmail.com>
2023-01-07 01:26:33 +0100

    Add source files

---
+++ ControlArduino.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    public class ControlArduino : MonoBehaviour
+    {
+        private CarController car;
+        private Rigidbody rb;
+        private SerialPort port = new SerialPort("COM3", 9600);
+        private int speed = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            car = GetComponent<CarController>();
+            port.Open();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            print(car.speed);
+            if (car.speed > 0)
+            {
+                speed = Convert.ToInt32(car.speed).ToString();
+                port.Write(speed);
+                print(speed);
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            port.Close();
+        }
+    }
+}
---
+++ LED_4DGame_ArduinoScript.ino
@@ -0,0 +1,26 @@
+int data = 0;
+int pin = 9;
+int strength = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  pinMode(pin, OUTPUT);
+}
+
+void loop() 
+{
+  analogWrite(pin, strength);
+  
+  if (Serial.available())
+  {
+    data = Serial.parseInt();
+    Serial.print(data);
+
+    if (data <= 255 || data >= 0)
+    {
+        strength = data;
+    }
+  }
+}
